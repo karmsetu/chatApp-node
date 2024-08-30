5a740583344261cd1fbd9afd8e4c37fa43657a7e
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2024-08-31 02:47:45 +0530

    react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Join from './components/Join';
+import Chat from './components/Chat';
 
-function App() {
-    return <></>;
-}
+const App = () => (
+    <Router>
+        <Routes>
+            <Route path="/" element={<Join />} />
+            <Route path="/chat" element={<Chat />} />
+        </Routes>
+    </Router>
+);
 
 export default App;
