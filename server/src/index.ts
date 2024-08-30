16d44011679f7ffe109855822ac3152faf6f02d9
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2024-08-31 03:09:14 +0530

    backend: socket connect & disconnect

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,28 @@
 // src/index.js
 import express, { Express, Request, Response } from 'express';
+import { Server as SocketIoServer } from 'socket.io';
+import { createServer } from 'node:http';
+import router from './router';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
+app.use(router);
 const port = process.env.PORT || 3000;
+const server = createServer(app);
+const io = new SocketIoServer(server);
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    res.send('<h1>hello!</h1>');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user left');
+    });
 });
 
 app.listen(port, () => {
